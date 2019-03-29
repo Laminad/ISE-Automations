dfe7af9b52ccbf9830322b3c5a2ba72b01c28c73
Dan Thompson <34109026+Laminad@users.noreply.github.com>
2019-03-29 14:01:48 -0400

    Add files via upload

--- mab_device_add.py
+++ mab_device_add.py
@@ -1,3 +1,70 @@
 class MABDeviceAdd(self):
 
+	import hashlib
+
 	def __init__(self):
+		self.endpoint_name = ''
+		self.endpoint_id = ''
+		self.endpoint_description = ''
+		self.group_id = ''
+		self.identity_store = ''
+		self.endpoint_mac_address = ''
+		self.portal_user = ''
+		self.profile_id = ''
+		
+	
+	def get_endpoint_name(self):
+		self.endpoint_name = input('Enter the Enpoint Name: ')
+		
+		
+	def get_endpoint_description(self):
+		self.endpoint_description = input('Enter a description for the Endpoint: ')
+		
+		
+	def get_group_id(self):
+		self.group_id = input('Enter the group ID of the Endpoint: ')
+		
+		
+	def get_identity_store(self):
+		self.i
+		dentity_store = input('Enter the Identity Store of the Endpoint: ')
+		
+	def get_endpoint_mac_address(self):
+		self.endpoint_mac_address = input('Enter the MAC address of the endpoint: ')
+		
+		
+	def get_portal_user(self):
+		self.portal_user = input('Enter your ISE API username: ')
+		
+		
+	def get_profile_id(self):
+		self.profile_id = input('Enter the Profile ID of the Enpoint: ')
+		
+		
+	def generate_endpoint_id(self):
+		self.endpoint_id = hashlib.sha256(self.endpoint_mac_address)
+		
+		
+	def enpoint_add_api_post_output(self):
+	
+		#Method: GET
+		#URI: https://ISE_IP_ADDRESS:9060/ers/config/endpoint
+		#HTTP Accept header:application/vnd.com.cisco.ise.identity.endpoint.1.0+xml 
+		
+		print(	
+		'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>',
+		'<ns3:endpoint',
+			'name='+self.endpoint_name,
+			'id='+self.endpoint_id,
+			'description='+self.endpoint_description,
+			'xmlns:ns2="ers.ise.cisco.com"',
+			'xmlns:ns3="identity.ers.ise.cisco.com">',
+			'<groupId>'+self.group_id+'</groupId>',
+			'<identityStore>'+self.identity_store+'</identityStore>',
+			'<identityStoreId>'self.identity_store+'</identityStoreId>',
+			'<mac>'+self.endpoint_mac_address+'</mac>',
+			'<portalUser>'+self.portal_user+'</portalUser>',
+			'<profileId>'+self.profile_id+'</profileId>',
+			'<staticGroupAssignment>true</staticGroupAssignment>',
+			'<staticProfileAssignment>false</staticProfileAssignment>',
+		'</ns3:endpoint>')
--- network_device_add.py
+++ network_device_add.py
@@ -1,6 +1,8 @@
 class NetworkDeviceAdd(self):
 
-import hashlib
+
+	import hashlib
+	
 	
 	def __init__(self):
 		self.device_id = ''
@@ -14,31 +16,45 @@ import hashlib
 		self.snmp_version = 'TWO_C'
 		self.snmp_community_string = ''
 		
+		
 	def get_device_hostname(self): 
 		self.device_hostname = input('Enter the device hostname: ')
+		
 		
 	def get_device_ip_address(self):
 		self.device_ip_address = input('Enter the device IP Address: ')
 	
+	
 	def get_ise_ip_address(self):
 		self.ise_ip_address = input('Enter the IP address of the ISE ERS enabled node: ')
+		
 		
 	def get_radius_shared_secert(self):
 		self.radius_shared_secert = input('Enter the RADIUS Shared Secert: ')	
 	
+	
 	def get_device_group(self):
 		self.device_group = input('Enter the Device Group: ')
+		
 		
 	def get_device_location(self):
 		self.device_location = input('Enter the device location: ')
 		
+		
 	def get_snmp_community_string(self):
 		self.snmp_community_string = input('Enter the device snmp community string: ')
+		
 		
 	def generate_device_id(self):
 		self.device_id = hashlib.sha256(self.device_hostname)
 		
+		
 	def print_network_add_api_post(self):
+	
+		#Method: POST
+		#URI: https://ISE_IP_ADDRESS:9060/ers/config/networkdevice
+		#Content-Type: application/vnd.com.cisco.ise.network.networkdevice.1.1+xml; charset=utf-8
+
 		print(
 		"<?xml version='1.0' encoding='UTF-8' standalone='yes'?>",
 			"<ns4:networkdevice", 
@@ -78,7 +94,7 @@ import hashlib
 		
 
 	if __name__ == '__main__':
-		#Collecting Device details
+		#Collecting Device details.
 		self.get_device_hostname()
 		self.get_device_ip_address()
 		self.get_ise_ip_address()
@@ -87,6 +103,8 @@ import hashlib
 		self.get_device_location()
 		self.get_snmp_community_string()
 		
-		#Using device details to input missing API values
+		#Using Device details to input missing API values.
 		self.generate_device_id()
 		self.print_network_add_api_post()
+		
+		
